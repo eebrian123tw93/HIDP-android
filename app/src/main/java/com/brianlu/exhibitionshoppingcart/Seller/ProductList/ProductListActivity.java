3efb798525c941ce50615f709426beaf90acc2e0
brian <eebrian123tw93@gmail.com>
2020-01-05 03:21:05 +0800

    pro

--- app/src/main/java/com/brianlu/exhibitionshoppingcart/Seller/ProductList/ProductListActivity.java
+++ app/src/main/java/com/brianlu/exhibitionshoppingcart/Seller/ProductList/ProductListActivity.java
@@ -1,10 +1,14 @@
 package com.brianlu.exhibitionshoppingcart.Seller.ProductList;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.brianlu.exhibitionshoppingcart.Buyer.ShoppingCart.ShoppingCartActivity;
+import com.brianlu.exhibitionshoppingcart.Identity.IdentityChooseActivity;
 import com.brianlu.exhibitionshoppingcart.R;
+import com.brianlu.exhibitionshoppingcart.Seller.AddProduct.AddProductActivity;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class ProductListActivity extends AppCompatActivity implements ProductListView {
@@ -15,7 +19,14 @@ public class ProductListActivity extends AppCompatActivity implements ProductLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        findViewById(R.id.add_product_button).setOnClickListener(view -> {
+            Intent intent = new Intent(ProductListActivity.this, AddProductActivity.class);
+            startActivity(intent);
+        });
+
         presenter = new ProductListPresenter(this);
+
     }
 
     @Override
